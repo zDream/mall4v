0bbe17f8c9565a5cf2dba000aff0ffbfd8773d61
ztt <zttongdeyx@126.com>
2020-10-20 16:37:37 +0800

    进行中

--- static/config/index-prod.js
+++ static/config/index-prod.js
@@ -7,7 +7,7 @@
   // api接口请求地址
   window.SITE_CONFIG['baseUrl'] = 'http://47.96.111.48:3000/apis'
   // 静态资源文件url
-  window.SITE_CONFIG['resourcesUrl'] = 'http://img-test.gz-yami.com/'
+  window.SITE_CONFIG['resourcesUrl'] = 'http://47.96.111.48:3000/apis/image/'
   // cdn地址 = 域名 + 版本号
   window.SITE_CONFIG['domain'] = './' // 域名
   window.SITE_CONFIG['version'] = ''   // 版本号(年月日时分)
